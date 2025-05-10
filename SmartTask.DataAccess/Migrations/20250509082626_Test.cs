9948ab0ee194f636a2bdb8ebf62772e81266b935
iMGamal <mohamed.gamal126027@gmail.com>
2025-05-10 14:51:59 +0300

    Handled users uploading their profile images while going through registration process. Moreover, handled authorization for any update to user data to be managed by an Admin or the account user himself / herself

---
+++ SmartTask.DataAccess/Migrations/20250509082626_Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartTask.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "AspNetUsers");
+        }
+    }
+}
